d1ec209a3896caaafa9ed20c5b08072a6f39dc48
Nitesh <Niteshmishra7385@gmail.com>
2023-02-07 15:57:43 +0530

    Initial commit

---
+++ StorageAccounts/Repsitory/BlobStorage.cs
@@ -0,0 +1,139 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Azure.Management.Storage.Fluent.Models;
+using Microsoft.WindowsAzure.Storage.Blob;
+using BlobProperties = Azure.Storage.Blobs.Models.BlobProperties;
+
+namespace StorageAccounts.Repsitory
+{
+    public class BlobStorage
+    {
+        static string connectionstring = "DefaultEndpointsProtocol=https;AccountName=storageiot333;AccountKey=/55ylqlPNbLFJn0LhhTyv9ktUzH8jC+Aao1YBc/Saq1etjNC+aVMKWi76khZh4851Mwxjg0xoEvU+AStGbtpbw==;EndpointSuffix=core.windows.net";
+        public static async Task CreateBlob(string blobName)
+        {
+            if (string.IsNullOrEmpty(blobName))
+            {
+                throw new ArgumentNullException("enter blob name");
+            }
+            try
+            {
+                BlobContainerClient container = new BlobContainerClient(connectionstring, blobName);
+                await container.CreateAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+            public static async Task DeleteBlob(string blobName)
+            {
+                if(string.IsNullOrEmpty(blobName))
+                {
+                    throw new ArgumentNullException("enter blob name");
+                }
+                try
+                {
+                    BlobContainerClient container = new BlobContainerClient(connectionstring, blobName);
+                    await container.DeleteAsync();
+                }
+                catch(Exception ex)
+                {
+                    throw ex;
+            }
+        }
+        public static async Task DeleteBlobContent(string blobName, string file)
+        {
+            if (string.IsNullOrEmpty(blobName))
+            {
+                throw new ArgumentNullException("enter blob name");
+            }
+            try
+            {
+                BlobContainerClient container = new BlobContainerClient(connectionstring, blobName);
+                await container.DeleteAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+   
+        public static async Task<BlobProperties> UpdateBlobContent(string blobName,string file)
+        {
+            if(string.IsNullOrEmpty(blobName))
+            {
+                throw new ArgumentNullException("enter blob name");
+            }
+            try
+            {
+                string fileName = Path.GetTempFileName();
+                BlobContainerClient container = new BlobContainerClient(connectionstring, blobName);
+                BlobClient blob = container.GetBlobClient(file);
+                await blob.UploadAsync(fileName);
+                BlobProperties prop = await blob.GetPropertiesAsync();
+                return prop;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static async Task<BlobProperties> getBlobContent(string blobName,string file)
+        {
+            if(string.IsNullOrEmpty(blobName))
+            {
+                throw new ArgumentException("enter blob name");
+            }
+            try
+            {
+                BlobContainerClient container = new BlobContainerClient(connectionstring,blobName);
+                BlobClient blob = container.GetBlobClient(file);
+                BlobProperties prop = await blob.GetPropertiesAsync();
+                return prop;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static async Task<List<string>> GetBlob(string blobName,string file)
+        {
+            if(string.IsNullOrEmpty(blobName))
+            {
+                throw new ArgumentException("enter blob name");
+            }
+            try
+            {
+                BlobContainerClient container = new BlobContainerClient(connectionstring, blobName);
+                List<string> names = new List<string>();
+               await foreach(BlobItem a in container.GetBlobsAsync())
+                {
+                    names.Add(a.Name);
+                }
+                return names;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static async Task<BlobProperties> DownloadBlob(string blobName,string file)
+        {
+            try
+            {
+                string path = "" + blobName;
+                BlobContainerClient container = new BlobContainerClient(connectionstring, blobName);
+                BlobClient client = container.GetBlobClient(file);
+                await client.DownloadToAsync(path);
+                BlobProperties prop= await client.GetPropertiesAsync();
+                return prop;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
+
---
+++ StorageAccounts/Repsitory/TableStorage.cs
@@ -0,0 +1,44 @@
+﻿using StorageAccounts.DTO_s;
+using Azure.Data.Tables;
+
+namespace StorageAccounts.Repsitory
+{
+    public class TableStorage
+    {
+        static string connectionstring = "DefaultEndpointsProtocol=https;AccountName=storageiot333;AccountKey=/55ylqlPNbLFJn0LhhTyv9ktUzH8jC+Aao1YBc/Saq1etjNC+aVMKWi76khZh4851Mwxjg0xoEvU+AStGbtpbw==;EndpointSuffix=core.windows.net";
+        public static async Task AddTable(string tableName)
+        {
+            var data = new TableServiceClient(connectionstring);
+            var client = data.GetTableClient(tableName);
+            await client.CreateIfNotExistsAsync();
+
+        }
+        public static async Task<Details> UpdateTable(Details employee,string tableName)
+        {
+            var data = new TableServiceClient(connectionstring);
+            var client = data.GetTableClient(tableName);
+            await client.UpsertEntityAsync(employee);
+            return null;
+        }
+        public static async Task<Details> GetTableData(string tableName,string partitionKey,string rowKey)
+        {
+            var data = new TableServiceClient(connectionstring);
+            var client = data.GetTableClient(tableName);
+            var tableData = await client.GetEntityAsync<Details>(partitionKey, rowKey);
+            return tableData;
+        }
+        public static async Task<TableClient> GetTable(string tableName)
+        {
+            var data = new TableServiceClient(connectionstring);
+            var client = data.GetTableClient(tableName);
+            return client;
+        }
+        public static async Task DeleteTableData(string tableName,string department,string id)
+        {
+            var data = new TableServiceClient(connectionstring);
+            var client=data.GetTableClient(tableName);
+            await client.DeleteEntityAsync(department, id);
+            return; 
+        }
+    }
+}
